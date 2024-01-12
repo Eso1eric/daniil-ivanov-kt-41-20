64afdf40f8ef0e1928c4a5abccd75b625dfd2b06
Eso1eric <dxcake@gmail.com>
2024-01-12 10:36:17 +0300

    - Миграции лабораторной 4

---
+++ daniil-ivanov-kt-41-20/Migrations/20240112061520_UpdateTeachers.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daniil_ivanov_kt_41_20.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTeachers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "cd_teachers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_cd_teachers_fk_f_department_id",
+                table: "cd_teachers",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_f_department_id",
+                table: "cd_teachers",
+                column: "DepartmentId",
+                principalTable: "cs_departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_f_department_id",
+                table: "cd_teachers");
+
+            migrationBuilder.DropIndex(
+                name: "idx_cd_teachers_fk_f_department_id",
+                table: "cd_teachers");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "cd_teachers");
+        }
+    }
+}
---
+++ daniil-ivanov-kt-41-20/Migrations/20240112072740_UpdateTeachersDepartments.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daniil_ivanov_kt_41_20.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTeachersDepartments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_f_headteacher_id",
+                table: "cs_departments");
+
+            migrationBuilder.DropIndex(
+                name: "idx_cs_departments_fk_f_headteacher_id",
+                table: "cs_departments");
+
+            migrationBuilder.DropColumn(
+                name: "HeadTeacherId",
+                table: "cs_departments");
+
+            migrationBuilder.RenameTable(
+                name: "Positions",
+                newName: "cs_positions");
+
+            migrationBuilder.RenameTable(
+                name: "Degrees",
+                newName: "cs_degrees");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "b_isHeadTeacher",
+                table: "cd_teachers",
+                type: "bool",
+                nullable: false,
+                defaultValue: false,
+                comment: "Заведующий кафедры");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "b_isHeadTeacher",
+                table: "cd_teachers");
+
+            migrationBuilder.RenameTable(
+                name: "cs_positions",
+                newName: "Positions");
+
+            migrationBuilder.RenameTable(
+                name: "cs_degrees",
+                newName: "Degrees");
+
+            migrationBuilder.AddColumn<int>(
+                name: "HeadTeacherId",
+                table: "cs_departments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_cs_departments_fk_f_headteacher_id",
+                table: "cs_departments",
+                column: "HeadTeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_f_headteacher_id",
+                table: "cs_departments",
+                column: "HeadTeacherId",
+                principalTable: "cd_teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
